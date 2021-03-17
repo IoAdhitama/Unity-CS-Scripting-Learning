7891e6d76bedff56cc21dd2bf3527dfb2b9c5cc1
Io Adhitama <44990840+IoAdhitama@users.noreply.github.com>
2021-03-17 12:17:52 +0700

    Initial commit

---
+++ Assets/Scripts/Properties/GameProperties.cs
@@ -0,0 +1,62 @@
+﻿// See PlayerProperties.cs for details on PlayerProperties class.
+// See PropertiesScene scene for usage of this script.
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameProperties : MonoBehaviour
+{
+    public Text health;
+    public Text experience;
+    public Text level;
+    public Text buttonPress;
+
+    PlayerProperties myPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myPlayer = new PlayerProperties();
+        myPlayer.Health = 100;
+
+        health.text = "Health: " + myPlayer.Health;
+        experience.text = "Experience: " + myPlayer.Experience;
+        level.text = "Level: " + myPlayer.Level;
+        buttonPress.text = "Button Press Amount: " + myPlayer.ButtonPress;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Gain experience when V is pressed
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            myPlayer.Experience += 500;
+            experience.text = "Experience: " + myPlayer.Experience;
+            level.text = "Level: " + myPlayer.Level;
+            IncrementButtonPressAmount();
+        }
+
+        // Gain health when H is pressed
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            myPlayer.Health += 10;
+            health.text = "Health: " + myPlayer.Health;
+            IncrementButtonPressAmount();
+        }
+
+        // Lose health when Z is pressed
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            myPlayer.Health -= 10;
+            health.text = "Health: " + myPlayer.Health;
+            IncrementButtonPressAmount();
+        }
+    }
+
+    // Increment the amount of times the key is pressed
+    void IncrementButtonPressAmount()
+    {
+        myPlayer.ButtonPress++;
+        buttonPress.text = "Button Press Amount: " + myPlayer.ButtonPress;
+    }
+}
---
+++ Assets/Scripts/Properties/PlayerProperties.cs
@@ -0,0 +1,55 @@
+﻿// See GameProperties.cs for usage of this class.
+public class PlayerProperties
+{
+    // This is a field (member variables)
+    private int experience = 0;
+    private int health;
+
+    // This is a basic/standard property, showing how much experience point the player have
+    public int Experience
+    {
+        get
+        {
+            return experience;
+        }
+
+        set
+        {
+            experience = value;
+        }
+    }
+
+    // Level property: Convert experience points into level (1000xp per level)
+    // This property is an example of a read-only property
+    public int Level 
+    {
+        get
+        {
+            return experience / 1000;
+        }
+    }
+
+    // Property to manage player's health
+    // This property shows that some other lines of codes can be inserted into the get and set method.
+    public int Health
+    {
+        get
+        {
+            return health;
+        }
+
+        // By inserting some if statement, we can restrict the range of the class variable,
+        // ensuring that the health will never go down below 0
+        set
+        {
+            if (value < 0)
+            {
+                health = 0;
+            }
+            else health = value;
+        }
+    }
+
+    // An auto-implemented property, used to track how many times a key is pressed
+    public int ButtonPress { get; set; }
+}
