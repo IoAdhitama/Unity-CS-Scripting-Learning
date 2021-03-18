a3235b3bdb6367b146cf852f61ee294118aa2a5f
Io Adhitama <44990840+IoAdhitama@users.noreply.github.com>
2021-03-18 10:55:06 +0700

    Add Unity Scene to demonstrate C# method overloading

---
+++ Assets/Scripts/MethodOverloading/GameOverload.cs
@@ -0,0 +1,49 @@
+﻿// See MethodOverloadScene for usage of this script.
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverload : MonoBehaviour
+{
+    private MethodOverload methodOverload;
+
+    private string[] wordList = {"Lorem", "Ipsum", "Dolor", "Sit", "Amet", "Consectetur", "Adipiscing", "Elit", "Hello", "World"};
+
+    public Text addedNumbersText;
+    public Text addedStringsText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        methodOverload = new MethodOverload();
+
+        addedNumbersText.text = "0 + 0 = 0";
+        addedStringsText.text = "Lorem + Ipsum = Lorem Ipsum";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int num1;
+        int num2;
+        
+        // When Z is pressed, generate two random numbers, then add it up
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            num1 = Random.Range(1, 100);
+            num2 = Random.Range(1, 100);
+
+            int resultNumber = methodOverload.Add(num1, num2);
+            addedNumbersText.text = num1 + " + " + num2 + " = " + resultNumber;
+        }
+
+        // When M is pressed, pick two random words from the wordList array, then add it up
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            num1 = Random.Range(0, wordList.Length);
+            num2 = Random.Range(0, wordList.Length);
+
+            string resultText = methodOverload.Add(wordList[num1], wordList[num2]);
+            addedStringsText.text = wordList[num1] + " + " + wordList[num2] + " = " + resultText;
+        }
+    }
+}
---
+++ Assets/Scripts/MethodOverloading/MethodOverload.cs
@@ -0,0 +1,16 @@
+﻿// See GameOverload.cs for usage of this class.
+// This class is a simple implementation of method overloading.
+public class MethodOverload
+{
+    // The first add method will add up two numbers together
+    public int Add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+
+    // The second add method will join two words together
+    public string Add(string word1, string word2)
+    {
+        return word1 + " " + word2;
+    }
+}
