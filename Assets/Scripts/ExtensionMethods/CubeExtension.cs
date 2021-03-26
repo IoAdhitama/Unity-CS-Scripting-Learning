333e6bbe66d38847e151335b6d613f33aa13b9e6
Io Adhitama <44990840+IoAdhitama@users.noreply.github.com>
2021-03-26 15:34:32 +0700

    Add C# extension method unity scene
    Add unity scene to demonstrate use of C# extension method scripting

---
+++ Assets/Scripts/ExtensionMethods/CubeExtension.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeExtension : MonoBehaviour
+{
+    public Vector3 destination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.ResetTransformation();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position != destination)
+        {
+            transform.MoveTo(destination, 5);
+        }
+    }
+}
---
+++ Assets/Scripts/ExtensionMethods/ExtensionMethods.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ExtensionMethods
+{
+    // Reset the transformation
+    public static void ResetTransformation(this Transform trans)
+    {
+        trans.position = Vector3.zero;
+        trans.localRotation = Quaternion.identity;
+        trans.localScale = Vector3.one;
+    }
+
+    // Move to a certain location
+    public static void MoveTo(this Transform source, Vector3 destination , float speed)
+    {
+        if (source.position != destination)
+        {
+            source.Translate((destination - source.position).normalized * speed * Time.deltaTime);
+        }
+        else { source.position = destination; }
+    }
+}
