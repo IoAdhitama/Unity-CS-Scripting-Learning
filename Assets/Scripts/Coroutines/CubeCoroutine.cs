c3f8fb66b7a136d493326518e11016f684bd49d1
Io Adhitama <44990840+IoAdhitama@users.noreply.github.com>
2021-03-29 10:31:19 +0700

    Add Coroutine demo Unity Scene
    Add unity scene to demonstrate usage of Coroutines in C#

---
+++ Assets/Scripts/Coroutines/CubeCoroutine.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeCoroutine : MonoBehaviour
+{
+    public Vector3 target;
+    public float speed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Movement(target));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Movement(Vector3 target)
+    {
+        while(Vector3.Distance(transform.position, target) > 0.05f)
+        {
+            transform.position = Vector3.Lerp(transform.position, target, speed * Time.deltaTime);
+
+            yield return null;
+        }
+    }
+}
