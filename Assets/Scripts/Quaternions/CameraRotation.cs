5e55d5620c96175a30558096c20c658ea968e33d
Io Adhitama <44990840+IoAdhitama@users.noreply.github.com>
2021-03-29 11:30:49 +0700

    Add unity scene for learning Quaternion

---
+++ Assets/Scripts/Quaternions/CameraRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    float rotationSpeed = 36f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Quaternions/LookAtScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtScript : MonoBehaviour
+{
+    public Transform lookTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 relativePos = transform.position - lookTarget.position;
+        transform.rotation = Quaternion.LookRotation(relativePos);
+    }
+}
