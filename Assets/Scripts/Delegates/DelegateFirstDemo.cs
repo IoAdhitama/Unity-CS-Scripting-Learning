c9246d3e09109e6e567a579aaa16576761764c16
Io Adhitama <44990840+IoAdhitama@users.noreply.github.com>
2021-03-29 14:35:47 +0700

    Add unity scene that uses delegates
    Add unity scene to demonstrate use of delegates in C# in its script

---
+++ Assets/Scripts/Delegates/DelegateFirstDemo.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelegateFirstDemo : MonoBehaviour
+{
+    delegate void MyDelegate(int num);
+    MyDelegate exampleDelegate;
+
+    Vector3 targetLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        exampleDelegate = SetX;
+        exampleDelegate(10);
+
+        exampleDelegate = SetZ;
+        exampleDelegate(10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.MoveTo(targetLocation, 1f);
+    }
+
+    void SetX(int num)
+    {
+        targetLocation.x = num;
+    }
+
+    void SetZ(int num)
+    {
+        targetLocation.z = num;
+    }
+
+}
---
+++ Assets/Scripts/Delegates/DelegateSecondDemo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelegateSecondDemo : MonoBehaviour
+{
+    delegate void Delegate();
+    Delegate exampleDelegate;
+
+    Vector3 targetLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        exampleDelegate += SetTargetLocation;
+        exampleDelegate += ChangeColor;
+
+        exampleDelegate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void SetTargetLocation()
+    {
+        targetLocation = new Vector3(-10, 0, 10);
+    }
+
+    void ChangeColor()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    void Move()
+    {
+        transform.MoveTo(targetLocation, 1f);
+    }
+}
